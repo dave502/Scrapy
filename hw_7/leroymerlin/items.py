207dc1f6319f65e9cb3645ad2b414f1e72c252b4
Dave <davtyan.d.n@mail.ru>
2021-08-23 10:06:35 +0200

    Update items.py

--- hw_7/leroymerlin/items.py
+++ hw_7/leroymerlin/items.py
@@ -8,7 +8,10 @@ import re
 
 
 def get_item_properties(value):
+    """возвращает пару ключ:значение из строки с характеристками товара"""
     try:
+        # вариант передачи в Item характеристик по строкам и дальнейшее их разбиение на ключ-значение мне показался
+        # более надёжным и предсказуемым (хоть и более затратным) чем парсинг всех элементов блока отдельно в один список
         return {value.split('<dt class="def-list__term">')[1].split('</dt>')[0].replace('\n', '').strip():
         value.split('<dd class="def-list__definition">')[1].split('</dd>')[0].replace('\n', '').strip()}
     except:
