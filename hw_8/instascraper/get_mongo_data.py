dfcafc3001587e45f0882a00ea23a14be600b178
Dave <davtyan.d.n@mail.ru>
2021-08-31 10:24:57 +0200

    Update get_mongo_data.py

--- hw_8/instascraper/get_mongo_data.py
+++ hw_8/instascraper/get_mongo_data.py
@@ -1,13 +1,23 @@
 from pymongo import MongoClient
 db_client = MongoClient('localhost', 27017)
 db_friendships = db_client.instadata.friendships
-# подписки
+# подписчики
+# тут можно было бы, конечно, сделать запросы покрасивее
+# ищем список id подписчиков в коллекции friendships
 followers = db_friendships.find({"followed_to": "38299443164"})
-print(followers)
+# ищем данные подписчиков в коллекции users
 db_users = db_client.instadata.users
 users = db_users.find({'_id': {'$in': [user['follower'] for user in followers]}})
 print('Подписчики:')
 for user in users:
     print(user['username'])
-# # подписки
+# подписки
+subscriptions = db_friendships.find({"follower": "38299443164"})
+# ищем данные подписок в коллекции users
+db_users = db_client.instadata.users
+users = db_users.find({'_id': {'$in': [user['followed_to'] for user in followers]}})
+print('Подписки:')
+for user in users:
+    print(user['username'])
+
 
